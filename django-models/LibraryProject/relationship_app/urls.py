984c904e0b24f461a0fe5835474b136cdb6caab4
42ndLight <leichteed@gmail.com>
2025-02-23 19:25:24 +0300

    fix(relationship_app): correct import for RegisterView in URL routing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView, RegisterView , LoginView, LogoutView
+from LibraryProject.relationship_app import views
 
 app_name = 'relationship_app'
 urlpatterns = [    
     path('lib/<int:pk>/', LibraryDetailView.as_view(), name='lib_detail'),
     path('books/', list_books, name='booklist'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
