36f544d5611368804ae738e55f3f302271c5cfe9
42ndLight <leichteed@gmail.com>
2025-03-01 17:12:45 +0300

    feat(bookshelf): add permissions for book model and refactor user creation methods

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,25 +9,35 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        permissions = [
+            ("can_view_book", "Can view book"),
+            ("can create_book", "Can create book"),
+            ("can_edit_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, username, email, password, date_of_birth, profile_photo):
+    def create_user(self, username, email, password, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, password=password, date_of_birth=date_of_birth, profile_photo=profile_photo)
-        user.set_password(password)  
-        user.date_of_birth = date_of_birth
-        user.profile_photo = profile_photo      
+        extra_fields.setdefault("is_active", True)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, date_of_birth, )
+        if extra_fields.get("is_staff") is not True or extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_staff=True and is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
 
         
 class CustomUser(AbstractUser):
@@ -36,11 +46,7 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'username'    
-    REQUIRED_FIELDS = ['password', 'date_of_birth']
-
-
-
+    
     def __str__(self):
         return self.email  
 
