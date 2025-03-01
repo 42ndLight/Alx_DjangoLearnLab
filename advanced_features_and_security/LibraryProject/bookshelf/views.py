64b62fcb19dbb4116a96a9e6d75b5c12b558d2b2
42ndLight <leichteed@gmail.com>
2025-03-01 17:23:16 +0300

    feat(bookshelf): add index view with permission check for book access

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.user.has_perm('bookshelf.can_view_book'):
+        return render(request, 'bookshelf/index.html', {'user': request.user})
+    else:
+        return render(request, 'bookshelf/permission_denied.html') 
+
