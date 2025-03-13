bb3183de0d62c9099f4ef709c716aa83e68a9c50
42ndLight <leichteed@gmail.com>
2025-03-13 14:13:36 +0300

    chore(api): update .gitignore and add django_filters to settings; remove redundant paths in urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,10 +4,10 @@ from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete
 
 
 urlpatterns = [
-    path('/books/', BookList.as_view(), name='book_list'),
-    path('/books/<int:pk>/', BookDetail.as_view(), name='book_detail'),
-    path('/books/create/', BookCreate.as_view(), name='book_create'),
-    path('/books/update/<int:pk>/', BookUpdate.as_view(), name='book_update'),
-    path('/books/delete/<int:pk>/', BookDelete.as_view(), name='book_delete'),
-    path('admin/', admin.site.urls),
+    path('books/', BookList.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetail.as_view(), name='book_detail'),
+    path('books/create/', BookCreate.as_view(), name='book_create'),
+    path('books/update/<int:pk>/', BookUpdate.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', BookDelete.as_view(), name='book_delete'),
+    path('admin/', admin.site.urls, name='admin'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,23 +3,43 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+    
+
 class BookList(ListView):
     model = Book
     template_name = 'book_list.html'
     context_object_name = 'books'
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author', 'price']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'price']
 
+    @api_view(['GET'])
+    @permission_classes([IsAuthenticated|ReadOnly])
     def book_list(request):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
+    
+
+
     
 class BookDetail(DetailView):
     model = Book
     template_name = 'book_detail.html'
     context_object_name = 'book'
 
+    @api_view(['GET'])
+    @permission_classes([IsAuthenticated|ReadOnly])
     def book_detail(request, pk):
         book = Book.objects.get(pk=pk)
         serializer = BookSerializer(book)
@@ -30,6 +50,8 @@ class BookCreate(CreateView):
     template_name = 'book_form.html'
     fields = ['title', 'author', 'pages', 'price']
 
+    @api_view(['POST'])
+    @permission_classes([IsAuthenticated])
     def book_create(request):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
@@ -42,6 +64,8 @@ class BookUpdate(UpdateView):
     template_name = 'book_form.html'
     fields = ['title', 'author', 'pages', 'price']
 
+    @api_view(['PUT'])
+    @permission_classes([IsAuthenticated])
     def book_update(request, pk):
         book = Book.objects.get(pk=pk)
         serializer = BookSerializer(instance=book, data=request.data)
@@ -55,6 +79,8 @@ class BookDelete(DeleteView):
     template_name = 'book_confirm_delete.html'
     success_url = '/'
 
+    @api_view(['DELETE'])
+    @permission_classes([IsAuthenticated])
     def book_delete(request, pk):
         book = Book.objects.get(pk=pk)
         book.delete()
