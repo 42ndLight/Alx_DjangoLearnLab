2c9020b21dd5c07d5153bde41a6b54e422b48324
42ndLight <leichteed@gmail.com>
2025-03-19 22:01:36 +0300

    feat(auth): implement custom user creation and authentication views with forms

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import SignUpView, profile_view
-
+from .views import profile_view, register, login, logout
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('register/', SignUpView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
     path('profile/', profile_view, name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,17 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'blog/signup.html'
+from .forms import CustomUserCreationForm, ProfileForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def logout(request):
+    logout(request)
+    return redirect('login')
 
 @login_required
 def profile_view(request):
-    return render(request, 'blog/profile.html')
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form}) 
