8ba7fa15da66d7201230ee62c7daf8b956de4364
42ndLight <leichteed@gmail.com>
2025-03-20 01:55:24 +0300

    feat(blog): update post URL patterns for consistency and load static in create template

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('profile/', profile_view, name='profile'),
     path('posts', ListPostView.as_view(), name='index'),
-    path('posts/<int:pk>', DetailPostView.as_view(), name='detail'),
-    path('posts/new/', CreatePostView.as_view(), name='create'),
-    path('posts/<int:pk>/edit/', UpdatePostView.as_view(), name='edit'),
-    path('posts/<int:pk>/delete/', DeletePostView.as_view(), name='delete'),
+    path('post/<int:pk>', DetailPostView.as_view(), name='detail'),
+    path('post/new/', CreatePostView.as_view(), name='create'),
+    path('post/<int:pk>/edit/', UpdatePostView.as_view(), name='edit'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete'),
 ]
