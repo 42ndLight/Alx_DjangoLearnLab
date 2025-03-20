4be7ae6978fbae43964db44ee39f1f1c7493aa2f
42ndLight <leichteed@gmail.com>
2025-03-20 09:37:29 +0300

    feat(blog): rename edit URL to update for clarity in post management

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('posts', ListPostView.as_view(), name='index'),
     path('post/<int:pk>', DetailPostView.as_view(), name='detail'),
     path('post/new/', CreatePostView.as_view(), name='create'),
-    path('post/<int:pk>/edit/', UpdatePostView.as_view(), name='edit'),
+    path('post/<int:pk>/update/', UpdatePostView.as_view(), name='edit'),
     path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete'),
 ]
