0b76f206df7fab1f53b290993a6eb0a4fa9c6abf
42ndLight <leichteed@gmail.com>
2025-03-25 18:47:33 +0300

    fixing wrong literals

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewset
+from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
-class PostViewSet(viewset.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
-class CommentViewSet(viewset.ModelViewSet):
-    queryset = Comment.objcts.all()
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts', include('accounts.urls')),
+    path('api/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
